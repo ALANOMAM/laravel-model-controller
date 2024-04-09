7d3e1eef71430e0265cf1ac578770df7a0ef95b2
alan_omam <omamalan@hotmail.it>
2024-04-09 12:19:44 +0200

    aggiunto la cartella guest per il controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/* per aggiungere la cartella necessaria per il controller
+   usare " php artisan make:controller Cartella/NomeController "
+   per convenzione si usa spesso "php artisan make:controller Guest/PageController"
+   anche se il nome della cartella e del file sono na scelta nostra
+
+   NB:la chiamata va fatta in un terminale diverso da quello del backend e frontend
+*/
+
 Route::get('/', function () {
 
     return view('home');
